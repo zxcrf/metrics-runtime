dc24f1abb666729e1f3155c35b19411ff4965958
liukun <liukun@asiainfo.com>
2025-11-28 10:21:28 +0800

    [fix]:[][env and config]

--- test_multi_timepoint.lua
+++ test_multi_timepoint.lua
@@ -11,7 +11,10 @@ request = function()
     local date1 = get_random_date()
     local date2 = get_random_date()
     local date3 = get_random_date()
-    
-    local body = string.format('{"kpiArray": ["KD9999"], "opTimeArray": ["%s", "%s", "%s"], "dimCodeArray": ["city_id", "county_id"], "dimConditionArray": [], "includeTargetData": false, "includeHistoricalData": false}', date1, date2, date3)
+    local date4 = get_random_date()
+    local date5 = get_random_date()
+    local date6 = get_random_date()
+
+    local body = string.format('{"kpiArray": ["KD9999"], "opTimeArray": ["%s", "%s", "%s","%s", "%s", "%s"], "dimCodeArray": ["city_id", "county_id"], "dimConditionArray": [], "includeTargetData": false, "includeHistoricalData": false}', date1, date2, date3, date4, date5, date6)
     return wrk.format(nil, nil, nil, body)
 end
